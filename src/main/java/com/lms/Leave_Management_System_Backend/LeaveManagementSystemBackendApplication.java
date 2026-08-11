574ae5965df6aaf2baeb2ca251b7ce6ab61fa3be
sarthakj1 <107681855+sarthakj1@users.noreply.github.com>
2026-08-11 12:39:07 +0530

    Initial project setup with pr checks.

---
+++ src/main/java/com/lms/Leave_Management_System_Backend/LeaveManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lms.Leave_Management_System_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveManagementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lms/Leave_Management_System_Backend/LeaveManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.Leave_Management_System_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
